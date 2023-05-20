d417546e97f95f2e0826a8f7838c3e04bb9cb11e
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2023-05-20 10:55:13 +0300

    wip on cosmic event generation

--- simulator/src/index.ts
+++ simulator/src/index.ts
@@ -17,6 +17,34 @@ enum EventType {
   Comet = "Comet",
 }
 
+enum EventSource {
+  MMT = "MMT",
+  GeminiObservatoryTelescopes = "Gemini Observatory Telescopes",
+  VLT = "Very Large Telescope",
+  SubaruTelescope = "Subaru Telescope",
+  LBT = "Large Binocular Telescope",
+  SALT = "Southern African Large Telescope",
+  HET = "Hobby-Eberly Telescope",
+  GranTeCan = "Gran Telescopio Canarias",
+  GMT = "The Giant Magellan Telescope",
+  TMT = "Thirty Meter Telescope",
+  ELT = "European Extremely Large Telescope",
+}
+
+const getRandomEventSourceEnumValue = () => {
+  const eventSources = Object.keys(EventSource);
+  const randomIndex = Math.floor(Math.random() * eventSources.length);
+  const randomEnumValue = eventSources[randomIndex];
+  return randomEnumValue;
+};
+
+const getRandomEventTypeEnumValue = () => {
+  const eventSources = Object.keys(EventType);
+  const randomIndex = Math.floor(Math.random() * eventSources.length);
+  const randomEnumValue = eventSources[randomIndex];
+  return randomEnumValue;
+};
+
 // interface CosmicEventInterface {
 //   eventTS: number;
 //   eventSource: string;
@@ -30,7 +58,7 @@ const getRandomNumberByMaxVal = (maxVal: number) => {
   return Math.floor(Math.random() * maxVal);
 };
 
-function generateValidDEC() {
+const generateValidDEC = () => {
   const minDEC = -90;
   const maxDEC = 90;
 
@@ -38,22 +66,17 @@ function generateValidDEC() {
   const randomDEC = Math.floor(Math.random() * (maxDEC - minDEC + 1)) + minDEC;
 
   return randomDEC;
-}
+};
 
 class CosmicEvent {
   eventTS: number;
   eventSource: string;
   ra: RALocationUnits;
   dec: DECLocationUnits;
-  eventType: EventType;
+  eventType: string;
   urgency: number;
 
-  constructor(
-    eventTS: number,
-    eventSource: string,
-    eventType: EventType,
-    urgency: number
-  ) {
+  constructor(eventTS: number, urgency: number) {
     this.ra = {
       minutes: getRandomNumberByMaxVal(59),
       hours: getRandomNumberByMaxVal(23),
@@ -64,9 +87,31 @@ class CosmicEvent {
       seconds: getRandomNumberByMaxVal(59),
       degrees: generateValidDEC(),
     };
-    this.eventType = eventType;
+    this.eventType = getRandomEventTypeEnumValue();
+    this.eventSource = getRandomEventSourceEnumValue();
     this.urgency = urgency;
-    this.eventSource = eventSource;
     this.eventTS = eventTS;
   }
 }
+
+const generateCosmicEvent = () => {};
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+const main = async () => {
+  let eventCounter = 0;
+  console.log("starting to generate events");
+  while (true) {
+    generateCosmicEvent();
+    eventCounter++;
+    sleep(1000);
+  }
+};
+
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch(() => {
+    console.error("ERROR OCCURED!!");
+  });
