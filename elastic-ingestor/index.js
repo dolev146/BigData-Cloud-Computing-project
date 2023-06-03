45ae872f677cb636f3858bc969f55b2ef35f6d2a
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2023-06-03 18:59:05 +0300

    finished ingestor

---
+++ elastic-ingestor/index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const { Kafka } = require("kafkajs");
+const elastic = require("./elastic");
+
+const kafka_connection = new Kafka({
+  clientId: "my-app",
+  brokers: [process.env.KAFKA_HOSTNAME || ""],
+  ssl: true,
+  sasl: {
+    mechanism: "scram-sha-512", // scram-sha-256 or scram-sha-512
+    username: process.env.USERNAME || "",
+    password: process.env.PASSWORD || "",
+  },
+});
+
+const consumer = kafka_connection.consumer({ groupId: "qhagfczs-" });
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({
+    topic: "qhagfczs-test-topic",
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    eachMessage: async (messagePayload) => {
+      const { topic, partition, message } = messagePayload;
+
+      elastic.esclient
+        .index({
+          index: elastic.index,
+          body: message.value,
+        })
+        .then((result) => {
+          console.log(`insert result : ${result.statusCode}, ${result.body}`);
+        })
+        .catch((err) => {
+          console.error(`insert error : ${err}`);
+        });
+
+      //   const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+      //   console.log(`- ${prefix} ${message.key}#${message.value}`);
+    },
+  });
+};
+
+const main = async () => {
+    const elasticIndex = await elastic.esclient.indices.exists({
+      index: elastic.index,
+    });
+
+    if (!elasticIndex.body) {
+      await elastic.createIndex(elastic.index);
+    }
+  
+  run()
+    .then((res) => console.log(res))
+    .catch((e) => console.error(e));
+
+};
+
+main().catch((e) => console.error(`error eccured! : ${e}`));
