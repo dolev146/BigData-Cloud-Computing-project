7b2b15d9446004adb54661a951aa3b6922f5585b
yaakovkhodorkovski <yaakov103@gmail.com>
2023-06-10 12:53:15 +0300

    now we are calling also the astroids by Id's or using browse

--- api-service/routers/nasaRouter.js
+++ api-service/routers/nasaRouter.js
@@ -29,3 +29,39 @@ router.post('/asteroids', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch asteroid data' });
   }
 });
+
+// Lookup a specific asteroid based on its NASA JPL small body id
+router.get('/asteroids/:asteroidId', async (req, res) => {
+  try {
+    const asteroidId = req.params.asteroidId;
+
+    const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/neo/${asteroidId}`, {
+      params: {
+        api_key: NASA_API_KEY,
+      },
+    });
+
+    const asteroidData = response.data;
+    res.json(asteroidData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch asteroid data' });
+  }
+});
+
+// Browse the overall asteroid dataset
+router.get('/asteroids/browse', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse/', {
+      params: {
+        api_key: NASA_API_KEY,
+      },
+    });
+
+    const asteroidData = response.data;
+    res.json(asteroidData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch asteroid data' });
+  }
+});
