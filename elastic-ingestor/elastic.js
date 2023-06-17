f0ba5f94d8f6ea842d2ed0f5f79a6a9932c929db
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2023-06-17 10:41:36 +0300

    added the ingestor to a container

--- elastic-ingestor/elastic.js
+++ elastic-ingestor/elastic.js
@@ -1,7 +1,9 @@
 const { Client } = require("@elastic/elasticsearch");
 const elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200";
-const esclient = new Client({ node: elasticUrl });
+let esclient;
 const index = "stars";
+const maxAttempts = 5;
+const timeout = 3000; // milliseconds
 
 /**
  * @function createIndex
@@ -18,9 +20,55 @@ async function createIndex(index) {
   }
 }
 
+async function isAlive() {
+  try {
+    await esclient.ping({ index, requestTimeout: 10000 });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function connectToElasticsearch() {
+  let attempts = 0;
+  let connected = false;
+
+  while (attempts < maxAttempts && !connected) {
+    attempts++;
+    try {
+      esclient = new Client({
+        node: elasticUrl,
+        maxRetries: 3,
+        requestTimeout: 10000,
+      });
+      await esclient.ping();
+      connected = true;
+      console.log("Successfully connected to Elasticsearch!");
+    } catch (error) {
+      console.log(`Connection attempt ${attempts} failed: ${error.message}`);
+    }
+    if (!connected) {
+      console.log(`Retrying in ${timeout}ms...`);
+      await new Promise((resolve) => setTimeout(resolve, timeout));
+    }
+  }
+
+  if (!connected) {
+    throw new Error(
+      `Failed to connect to Elasticsearch after ${maxAttempts} attempts.`
+    );
+  }
+  return true;
+}
+
+function getClient(){
+  return esclient
+}
 
 module.exports = {
-  esclient,
+  getClient,
   createIndex,
   index,
+  isAlive,
+  connectToElasticsearch,
 };
