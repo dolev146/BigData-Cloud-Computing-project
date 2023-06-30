d717a84a8fbc9b907dc02897c103317e1b0cee2d
Dolev Dublon <127298010+ddublon@users.noreply.github.com>
2023-06-30 16:38:18 +0300

    feat: scraper

--- frontend/src/pages/Scraper.jsx
+++ frontend/src/pages/Scraper.jsx
@@ -1,27 +1,105 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Card, Button } from "antd";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { format, parse } from "date-fns";
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+`;
+
+const Image = styled.img`
+  max-width: 20vw;
+`;
+
+const TwoImageCard = styled(Card)`
+  .switch-button {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+`;
 
 const Scraper = () => {
   const [images, setImages] = useState({});
+  const [graphData, setGraphData] = useState([]);
+  const [activeImage, setActiveImage] = useState('sunspot-regions.jpg');
 
   useEffect(() => {
-    fetch("http://localhost:5001/api/scraper/all-images")
+    fetch("http://localhost:5001/api/scraper/all-images-and-aria-labels")
       .then((res) => res.json())
       .then((data) => {
         setImages(data.images);
+
+        const transformedGraphData = data.graphData.map((entry) => {
+          const lastCommaIndex = entry.lastIndexOf(",");
+          const timestampStr = entry.substring(0, lastCommaIndex).trim();
+          const date = parse(timestampStr, "EEEE, dd MMM., HH:mm", new Date());
+          const timestamp = format(date, "HH:mm");
+          return {
+            timestamp,
+            value: parseFloat(entry.substring(lastCommaIndex + 1)),
+          };
+        });
+
+        setGraphData(transformedGraphData);
       });
   }, []);
 
+  const switchImage = () => {
+    setActiveImage(activeImage === 'sunspot-regions.jpg' ? 'theskylivesun.jpg' : 'sunspot-regions.jpg');
+  };
+
   return (
-    <div>
-      Scraper
-      {Object.entries(images).map(([name, base64Img]) => (
-        <div key={name}>
-          <img src={`data:image/jpeg;base64,${base64Img}`} alt={name} />
-          <p>{name}</p>
-        </div>
-      ))}
-    </div>
+    <GridWrapper>
+      <Card title="Space Weather Sun">
+        <Image alt="spaceweathersun" src={`data:image/jpeg;base64,${images['spaceweathersun.jpg']}`} />
+      </Card>
+      <Card title="Solar Flares">
+        <Image alt="solarflares" src={`data:image/jpeg;base64,${images['solarflares.jpg']}`} />
+      </Card>
+      <TwoImageCard title={activeImage === 'sunspot-regions.jpg' ? 'Sunspot Regions' : 'The Sky Live Sun'}>
+        <Button className="switch-button" onClick={switchImage}>Switch Image</Button>
+        <Image alt="sunspot/skylive" src={`data:image/jpeg;base64,${images[activeImage]}`} />
+      </TwoImageCard>
+      <Card title="Coronal Mass">
+        <Image alt="coronal-mass" src={`data:image/jpeg;base64,${images['coronal-mass.jpg']}`} />
+      </Card>
+      <div>
+        <LineChart
+          width={600}
+          height={400}
+          data={graphData}
+          margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" padding={{ right: 30 }} />
+          <YAxis dataKey="value" domain={[0.000001, "dataMax"]} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="value" stroke="#ec7967" strokeWidth={2} activeDot={{ r: 6 }} />
+        </LineChart>
+      </div>
+      <Card title="Far Side">
+        <Image alt="far-side" src={`data:image/jpeg;base64,${images['far-side.jpg']}`} />
+      </Card>
+    </GridWrapper>
   );
 };
 
 export default Scraper;
+
+
+
+
+
--- scraper/routes/apiScraper.js
+++ scraper/routes/apiScraper.js
@@ -1,76 +1,73 @@
 import express from "express";
 const router = express.Router();
-
 import puppeteer from "puppeteer";
 import fetch from "node-fetch";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-router.get("/all-images", async (req, res) => {
-    const browser = await puppeteer.launch();
-  
-    // Fetch images from spaceweatherlive.com
-    let page = await browser.newPage();
-    await page.goto("https://www.spaceweatherlive.com/en/solar-activity.html");
-  
-    const images = [
-      { selector: "#SDO_512_0193", filename: "spaceweathersun.jpg" },
-      { selector: "#SDO_HMIIF_512", filename: "sunspot-regions.jpg" },
-      { selector: "#LASCO_C2", filename: "coronal-mass.jpg" },
-      { selector: "#SDO_512_0131", filename: "solarflares.jpg" },
-      { selector: "#EUVI195", filename: "far-side.jpg" },
-    ];
-  
-    let responseImages = {};
-  
-    for (const img of images) {
-      const imgUrl = await page.evaluate((selector) => {
-        const img = document.querySelector(selector);
-        return img.src;
-      }, img.selector);
-  
-      console.log(`Image source URL: ${imgUrl}`);
-  
-      // Download the image
-      const response = await fetch(imgUrl);
-      const buffer = await response.arrayBuffer();
-  
-      // Encode the image to Base64
-      const base64Image = Buffer.from(buffer).toString("base64");
-  
-      // Store in the responseImages object
-      responseImages[img.filename] = base64Image;
-    }
-  
-    // Fetch image from theskylive.com
-    page = await browser.newPage();
-    await page.goto("https://theskylive.com/sun-info");
-  
-    const theskylivesunImgUrl = await page.evaluate(() => {
-      const img = document.querySelector(".sun_container img");
+router.get("/all-images-and-aria-labels", async (req, res) => {
+  const browser = await puppeteer.launch();
+
+  // Navigate to spaceweatherlive.com
+  const page = await browser.newPage();
+  await page.goto("https://www.spaceweatherlive.com/en/solar-activity.html");
+
+  const images = [
+    { selector: "#SDO_512_0193", filename: "spaceweathersun.jpg" },
+    { selector: "#SDO_HMIIF_512", filename: "sunspot-regions.jpg" },
+    { selector: "#LASCO_C2", filename: "coronal-mass.jpg" },
+    { selector: "#SDO_512_0131", filename: "solarflares.jpg" },
+    { selector: "#EUVI195", filename: "far-side.jpg" },
+  ];
+
+  let responseImages = {};
+
+  for (const img of images) {
+    const imgUrl = await page.evaluate((selector) => {
+      const img = document.querySelector(selector);
       return img.src;
-    });
-  
-    console.log(`Image source URL: ${theskylivesunImgUrl}`);
-  
+    }, img.selector);
+
+    console.log(`Image source URL: ${imgUrl}`);
+
     // Download the image
-    const response = await fetch(theskylivesunImgUrl);
+    const response = await fetch(imgUrl);
     const buffer = await response.arrayBuffer();
-  
+
     // Encode the image to Base64
     const base64Image = Buffer.from(buffer).toString("base64");
-  
+
     // Store in the responseImages object
-    responseImages["theskylivesun.jpg"] = base64Image;
-  
-    browser.close();
-  
-    // Send the response
-    res.status(200).json({ images: responseImages });
+    responseImages[img.filename] = base64Image;
+  }
+
+  // Extract 'aria-label' values
+  const ariaLabels = await page.$$eval("path[aria-label]", (elements) =>
+    elements.map((item) => item.getAttribute("aria-label"))
+  );
+
+  // Remove "GOES-16 (Primary)." from each label and trim any additional whitespace
+  let cleanedLabels = ariaLabels.map((label) =>
+    label.replace("GOES-16 (Primary).", "").trim()
+  );
+
+  cleanedLabels = cleanedLabels.slice(0, -3);
+
+  // Navigate to theskylive.com using the same page object
+  await page.goto("https://theskylive.com/sun-info");
+
+  const theskylivesunImgUrl = await page.evaluate(() => {
+    const img = document.querySelector(".sun_container img");
+    return img.src;
   });
+  console.log(`Image source URL: ${theskylivesunImgUrl}`);
+  // Download the image
+  const response = await fetch(theskylivesunImgUrl);
+  const buffer = await response.arrayBuffer();
+  // Encode the image to Base64
+  const base64Image = Buffer.from(buffer).toString("base64");
+  // Store in the responseImages object
+  responseImages["theskylivesun.jpg"] = base64Image;
+  await browser.close();
+  res.status(200).json({ images: responseImages, graphData: cleanedLabels });
+});
 
 export default router;
