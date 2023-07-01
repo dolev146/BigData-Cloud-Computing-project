56bec4d68016c3fcdcf2e8336a5232f33369a94e
Dolev Dublon <127298010+ddublon@users.noreply.github.com>
2023-07-01 16:45:37 +0300

    fix: loader

--- frontend/src/pages/Nasa/NasaComponent.jsx
+++ frontend/src/pages/Nasa/NasaComponent.jsx
@@ -3,6 +3,17 @@ import Browse from "./Browse";
 import DateSearch from "./DateSearch";
 import RadioButtons from "./RadioButtons";
 import SearchById from "./SearchById";
+import styled from "styled-components";
+import { Radio } from "antd";
+
+const StyledHeader = styled.header`
+  text-align: center;
+  padding: 2rem;
+  font-size: 2.5rem;
+  color: #001529;
+  background: #f0f2f5;
+  margin-bottom: 2rem;
+`;
 
 const NasaComponent = () => {
   const [radioButtons, setRadioButtons] = useState({
@@ -13,13 +24,38 @@ const NasaComponent = () => {
 
   return (
     <div>
-      <RadioButtons
-        radioButtons={radioButtons}
-        setRadioButtons={setRadioButtons}
-      />
+      <StyledHeader>Near Earth Objects Dashboard</StyledHeader>
+      <Radio.Group
+        buttonStyle="solid"
+        style={{
+          marginBottom: "1rem",
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+        onChange={(e) =>
+          setRadioButtons({
+            searchById: e.target.value === "searchById",
+            browse: e.target.value === "browse",
+            dateSearch: e.target.value === "dateSearch",
+          })
+        }
+        value={
+          radioButtons.searchById
+            ? "searchById"
+            : radioButtons.browse
+            ? "browse"
+            : "dateSearch"
+        }
+      >
+        <Radio.Button value="dateSearch">Date Search</Radio.Button>
+        <Radio.Button value="searchById">Search By ID</Radio.Button>
+        <Radio.Button value="browse">Browse</Radio.Button>
+      </Radio.Group>
+
+      {radioButtons.dateSearch ? <DateSearch /> : null}
       {radioButtons.searchById ? <SearchById /> : null}
       {radioButtons.browse ? <Browse /> : null}
-      {radioButtons.dateSearch ? <DateSearch /> : null}
     </div>
   );
 };
