298a8ca0e4c7dd853b7602be6fae86c00ea431b9
yaakovkhodorkovski <yaakov103@gmail.com>
2023-07-01 13:40:16 +0300

    browse works

--- frontend/src/pages/Nasa/Browse.jsx
+++ frontend/src/pages/Nasa/Browse.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table, Tag } from 'antd';
 
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Browse
+  useEffect(() => {
+    axios.get('http://localhost:9080/nasa-api/asteroids/browse')
+      .then((response) => {
+        setData(response.data.near_earth_objects);
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }, []);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'NASA JPL URL',
+      dataIndex: 'nasa_jpl_url',
+      key: 'nasa_jpl_url',
+    },
+    {
+      title: 'Is Potentially Hazardous Asteroid',
+      dataIndex: 'is_potentially_hazardous_asteroid',
+      key: 'is_potentially_hazardous_asteroid',
+      render: isPotentiallyHazardous => (
+        <Tag color={isPotentiallyHazardous ? 'red' : 'green'}>
+          {isPotentiallyHazardous ? 'Yes' : 'No'}
+        </Tag>
+      ),
+    },
+    {
+      title: 'Absolute Magnitude H',
+      dataIndex: 'absolute_magnitude_h',
+      key: 'absolute_magnitude_h',
+    },
+    {
+      title: 'Estimated Diameter Min (km)',
+      dataIndex: ['estimated_diameter', 'kilometers', 'estimated_diameter_min'],
+      key: 'estimated_diameter_min',
+    },
+    {
+      title: 'Estimated Diameter Max (km)',
+      dataIndex: ['estimated_diameter', 'kilometers', 'estimated_diameter_max'],
+      key: 'estimated_diameter_max',
+    },
+    {
+      title: 'Orbit Id',
+      dataIndex: ['orbital_data', 'orbit_id'],
+      key: 'orbit_id',
+    },
+    // Add more columns as needed
+  ];
+
+  return (
+    <Table columns={columns} dataSource={data} rowKey='id' />
+  );
+};
+
+export default Browse;
