d4bafeea1e3670e2d8ba33123f126995eff23cb0
Dolev Dublon <127298010+ddublon@users.noreply.github.com>
2023-07-27 00:39:50 +0300

    alertsfix

---
+++ frontend/src/pages/Simulator/Alert2Component.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled, { keyframes, css } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faX,
+  faCircleExclamation,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+
+const AlertContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 1rem;
+  align-items: center;
+  background-color: ${(props) =>
+    props.type === "error" ? "#FFF2F0" : "#FFFBE6"};
+  border: ${(props) =>
+    props.type === "error" ? "1px solid #FFCCC7" : "1px solid #FFE58F"};
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+  padding: 1rem;
+`;
+
+const MessageContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-gap: 0.5rem;
+`;
+
+const TitleContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: center;
+`;
+
+// Create the styled component
+const DescriptionContainer = styled.div`
+  border-radius: 0.5rem;
+  color: ${(props) => (props.type === "error" ? "#FF4D4F" : "#FFA940")};
+  font-weight: 500;
+`;
+
+const Alert2Component = ({
+  message,
+  description,
+  type,
+  index,
+  onCloseFnc,
+}) => {
+  const handleClose = () => {
+    onCloseFnc(index);
+  };
+
+  return (
+    <AlertContainer type={type} >
+      {type === "error" ? (
+        <FontAwesomeIcon
+          icon={faCircleExclamation}
+          style={{ color: "#FF4D4F" }}
+          size="2xl"
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faTriangleExclamation}
+          style={{ color: "#FFA940" }}
+          size="2xl"
+        />
+      )}
+      <MessageContainer>
+        <TitleContainer>
+          <h3>{message}</h3>
+          <FontAwesomeIcon
+            icon={faX}
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              console.log("clicked");
+              handleClose(index);
+            }}
+          />
+        </TitleContainer>
+        <DescriptionContainer type={type}>{description}</DescriptionContainer>
+      </MessageContainer>
+    </AlertContainer>
+  );
+};
+
+export default Alert2Component;
