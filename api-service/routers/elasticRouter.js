6003fe33bac3f7340cb29c99eb1627be4a2e49a5
EitanKatsightfull <121748185+EitanKatsightfull@users.noreply.github.com>
2023-07-27 22:28:20 +0300

    fixed graph, now displays correct details

--- api-service/routers/elasticRouter.js
+++ api-service/routers/elasticRouter.js
@@ -2,173 +2,194 @@ import { Router } from "express";
 import { Client } from "@elastic/elasticsearch";
 
 const router = Router();
-const client = new Client({ node: "http://elasticsearch:9200" });
+const ELASTIC_SEARCH_URL = process.env.ELASTICSEARCH_URL || "http://localhost:9200";
+const client = new Client({ node: ELASTIC_SEARCH_URL });
 
 // Search by 2 date ranges
 router.get("/searchByDateRange", async (req, res) => {
-  const { startDate, endDate } = req.query;
-  console.log(startDate, endDate);
-  try {
-    const response = await client.search({
-      index: "stars",
-      body: {
-        query: {
-          range: {
-            eventTS: {
-              gte: startDate,
-              lte: endDate,
-            },
-          },
-        },
-      },
-    });
-    console.log(response.body);
-    res.json(response.body);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
-  }
+	const { startDate, endDate } = req.query;
+	
+	try {
+		const response = await client.search({
+			index: "stars",
+			body: {
+				query: {
+					range: {
+						eventTS: {
+							gte: startDate,
+							lte: endDate,
+						},
+					},
+				},
+			},
+		});
+		res.json(response.body);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message });
+	}
 });
 
 router.get("/searchByObservatoryAndDate", async (req, res) => {
-  const startDate = parseInt(req.query.startDate.trim(), 10);
-  const endDate = parseInt(req.query.endDate.trim(), 10);
-  const observatoryName = req.query.observatoryName.trim();
-
-  console.log(
-    "start Date,",
-    startDate,
-    "end date ",
-    endDate,
-    "observatory name",
-    observatoryName
-  );
-
-  try {
-    const response = await client.search({
-      index: "stars",
-      body: {
-        query: {
-          bool: {
-            must: [
-              {
-                match: {
-                  eventSource: observatoryName,
-                },
-              },
-              {
-                range: {
-                  eventTS: {
-                    gte: startDate,
-                    lte: endDate,
-                  },
-                },
-              },
-            ],
-          },
-        },
-      },
-    });
-    console.log(response.body);
-    res.json(response.body);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message });
-  }
+	const startDate = parseInt(req.query.startDate.trim(), 10);
+	const endDate = parseInt(req.query.endDate.trim(), 10);
+	const observatoryName = req.query.observatoryName.trim();
+	try {
+		const response = await client.search({
+			index: "stars",
+			body: {
+				query: {
+					bool: {
+						must: [
+							{
+								match: {
+									eventSource: observatoryName,
+								},
+							},
+							{
+								range: {
+									eventTS: {
+										gte: startDate,
+										lte: endDate,
+									},
+								},
+							},
+						],
+					},
+				},
+			},
+		});
+		res.json(response.body);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message });
+	}
 });
 
 // Search by ID and 2 date ranges
 router.get("/searchByIdAndDate", async (req, res) => {
-  const { id, startDate, endDate } = req.query;
+	const { id, startDate, endDate } = req.query;
 
-  // Convert date to timestamp
-  const startDateTimestamp = new Date(startDate).getTime();
-  const endDateTimestamp = new Date(endDate).getTime();
+	// Convert date to timestamp
+	const startDateTimestamp = new Date(startDate).getTime();
+	const endDateTimestamp = new Date(endDate).getTime();
 
-  try {
-    const response = await client.search({
-      index: "stars",
-      body: {
-        query: {
-          bool: {
-            filter: [
-              { term: { _id: id } },
-              {
-                range: {
-                  "_source.eventTS": {
-                    gte: startDateTimestamp,
-                    lte: endDateTimestamp,
-                  },
-                },
-              },
-            ],
-          },
-        },
-      },
-    });
-    res.json(response.body);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		const response = await client.search({
+			index: "stars",
+			body: {
+				query: {
+					bool: {
+						filter: [
+							{ term: { _id: id } },
+							{
+								range: {
+									"_source.eventTS": {
+										gte: startDateTimestamp,
+										lte: endDateTimestamp,
+									},
+								},
+							},
+						],
+					},
+				},
+			},
+		});
+		res.json(response.body);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
 // Get 4 stat numbers
 router.get("/stats", async (req, res) => {
-  try {
-    // const response = await axios.post(url, data);
-    /* send me a json looks like this { number1 : 50 , number2 : 90 , number3 : 20 , number4 : 70 } */
-    /* TODO: first number Total Number of Events */
-    /* TODO: second number Total Number of Dangerous Events */
-    /* TODO: third number needs to be Number of Events in the Last 24 Hours */
-    /* TODO: fourth number needs to be Number of Events in the Last 7 Days */
-    // res.json(response.data);
-    res.json({ number1: 50, number2: 90, number3: 20, number4: 70 });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+	try {
+		// const response = await axios.post(url, data);
+		/* send me a json looks like this { number1 : 50 , number2 : 90 , number3 : 20 , number4 : 70 } */
+		/* TODO: first number Total Number of Events */
+		/* TODO: second number Total Number of Dangerous Events */
+		/* TODO: third number needs to be Number of Events in the Last 24 Hours */
+		/* TODO: fourth number needs to be Number of Events in the Last 7 Days */
+		// res.json(response.data);
+		res.json({ number1: 50, number2: 90, number3: 20, number4: 70 });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
+const getESData = async (index, query) => {
+	const aggregationTerm = {
+		eventSource: {
+			terms: { field: "eventSource.keyword" },
+		},
+	};
+};
+
 router.get("/observatories", async (req, res) => {
-  try {
-    // const result = await client.search({
-    //   index: "observatories",
-    //   body: {
-    //     // define your query here
-    //     // you might use aggregations to get the count of events and dangerous events per observatory
-    //   }
-    // });
+	try {
+		const mapping = {};
+		const responseAggregate = await client.search({
+			index: "stars", // Replace 'Stars' with your actual index name
+			size: 0, // Set to 0 to only retrieve aggregation results without actual documents
+			body: {
+				aggs: {
+					eventSource: {
+						terms: { field: "eventSource.keyword" },
+					},
+				},
+			},
+		});
 
-    // process the result to form an array of observatory data
-    // const observatories = result.body.aggregations.observatories.buckets.map(
-    //   (bucket) => ({
-    //     ObservatoryName: bucket.key,
-    //     NumberOfEvents: bucket.events.doc_count,
-    //     NumberOfDangerousEvents: bucket.dangerousEvents.doc_count
-    //   })
-    // );
+		const responseDangerous = await client.search({
+			index: "stars", // Replace 'Stars' with your actual index name
+			size: 0, // Set to 0 to only retrieve aggregation results without actual documents
+			body: {
+				query: {
+					range: {
+						urgency: { gte: 3 }, // Filter events with urgency greater than 3
+					},
+				},
+				aggs: {
+					eventSourceUrgent: {
+						terms: { field: "eventSource.keyword" },
+					},
+				},
+			},
+		});
 
-    // res.json(observatories);
-    res.json([
-      {
-        ObservatoryName: "observatory1",
-        NumberOfEvents: 50,
-        NumberOfDangerousEvents: 20,
-      },
-      {
-        ObservatoryName: "observatory2",
-        NumberOfEvents: 30,
-        NumberOfDangerousEvents: 10,
-      },
-      {
-        ObservatoryName: "observatory3",
-        NumberOfEvents: 20,
-        NumberOfDangerousEvents: 5,
-      },
-    ]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred while querying the database");
-  }
+		const {
+			body: {
+				aggregations: { eventSource },
+			},
+		} = responseAggregate;
+
+		for (const bucketObj of eventSource.buckets) {
+			if (bucketObj?.key) {
+				mapping[bucketObj?.key] = {
+					ObservatoryName: bucketObj?.key?.slice(0, 5),
+					NumberOfEvents: bucketObj?.doc_count,
+					NumberOfDangerousEvents: 0,
+				};
+			}
+		}
+
+		const {
+			body: {
+				aggregations: { eventSourceUrgent },
+			},
+		} = responseDangerous;
+
+		for (const bucketObj of eventSourceUrgent.buckets) {
+			if (bucketObj?.key && mapping[bucketObj?.key]) {
+				mapping[bucketObj?.key].NumberOfDangerousEvents = bucketObj?.doc_count;
+			}
+		}
+
+		res.json(Object.values(mapping));
+	} catch (error) {
+		console.error(error);
+		res.status(500).send([]);
+	}
 });
 
 export { router };
