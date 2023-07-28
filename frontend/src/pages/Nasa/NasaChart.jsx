9e849ebf37b9673803d1f80da157ed4f0868c605
yaakovkhodorkovski <yaakov103@gmail.com>
2023-07-28 14:10:09 +0300

    cache with timestamp

--- frontend/src/pages/Nasa/NasaChart.jsx
+++ frontend/src/pages/Nasa/NasaChart.jsx
@@ -10,52 +10,55 @@ const ChartContainer = styled.div`
 `;
 
 const NasaChart = () => {
-  const [chartData, setChartData] = useState([]);
-
-  const fetchDataForWeek = async (start_date, end_date) => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ start_date, end_date }),
-    };
-
-    const response = await fetch('http://localhost:9080/nasa-api/asteroids', requestOptions);
-    const data = await response.json();
-    let weekData = {};
-
-    for (const [date, dataForDate] of Object.entries(data.near_earth_objects)) {
-      weekData[date] = dataForDate.length;
-    }
-
-    return weekData;
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let data = {};
-
-      for (let i = 0; i < 4; i++) {
-        const start_date = moment().subtract(i * 7, 'days').format('YYYY-MM-DD');
-        const end_date = moment().subtract((i + 1) * 7 - 1, 'days').format('YYYY-MM-DD');
-        const weekData = await fetchDataForWeek(start_date, end_date);
-        data = { ...data, ...weekData };
+    const [chartData, setChartData] = useState([]);
+  
+    const fetchDataForWeek = async (start_date, end_date) => {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ start_date, end_date }),
+      };
+  
+      const response = await fetch('http://localhost:9080/nasa-api/asteroids', requestOptions);
+      const data = await response.json();
+      let weekData = {};
+  
+      for (const [date, dataForDate] of Object.entries(data.near_earth_objects)) {
+        weekData[date] = dataForDate.length;
       }
-
-      const sortedData = Object.keys(data)
-        .sort((a, b) => new Date(a) - new Date(b))
-        .map(date => ({ date, 'Number of Events': data[date] }));
-
-      localStorage.setItem('chartData', JSON.stringify(sortedData));
-      setChartData(sortedData);
+  
+      return weekData;
     };
-
-    let storedData = localStorage.getItem('chartData');
-    if (storedData) {
-      setChartData(JSON.parse(storedData));
-    } else {
-      fetchData();
-    }
-  }, []);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        let data = {};
+  
+        for (let i = 0; i < 4; i++) {
+          const start_date = moment().subtract(i * 7, 'days').format('YYYY-MM-DD');
+          const end_date = moment().subtract((i + 1) * 7 - 1, 'days').format('YYYY-MM-DD');
+          const weekData = await fetchDataForWeek(start_date, end_date);
+          data = { ...data, ...weekData };
+        }
+  
+        const sortedData = Object.keys(data)
+          .sort((a, b) => new Date(a) - new Date(b))
+          .map(date => ({ date, 'Number of Events': data[date] }));
+  
+        localStorage.setItem('chartData', JSON.stringify(sortedData));
+        localStorage.setItem('fetchTime', JSON.stringify(new Date().getTime()));
+        setChartData(sortedData);
+      };
+  
+      let storedData = localStorage.getItem('chartData');
+      let fetchTime = JSON.parse(localStorage.getItem('fetchTime'));
+  
+      if (storedData && fetchTime && new Date().getTime() - fetchTime < 24 * 60 * 60 * 1000) {
+        setChartData(JSON.parse(storedData));
+      } else {
+        fetchData();
+      }
+    }, []);  
 
   return (
     <Card title="Asteroids Near Earth Distribution Chart" style={{marginBottom:"2dvh"}}> 
